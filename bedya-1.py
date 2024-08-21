81cf3b567dbf0c32a797c20c864c7115a066e18f
miguelcoronado003 <montanomiguel2013@gmail.com>
2024-08-21 11:53:56 -0500

    Numero par

--- bedya-1.py
+++ bedya-1.py
@@ -2,6 +2,13 @@ print ("hola mundo")
 numero = int(input("escriba un numero para saber si es positivo: "))
 if numero > 0 :
     print ("es positivo")
-
+    
 else:
     print("es negativo")
+    
+numero = int(input("escriba si el numero es par:"))
+if numero %2 == 0 :
+    print("numero es  par")
+    
+else:
+    print("no es par")    
